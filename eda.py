947b1409768d4f3e6b88c97fc68ae02edf4e7d65
Katerina Schulz <katerina@newton.local>
2019-10-11 13:12:02 -0700

    added idfs func

--- eda.py
+++ eda.py
@@ -21,11 +21,15 @@ def get_common_words(df):
     most_common_exc = Counter(text_body_exclusive).most_common(20)
     return most_common, most_common_exc
 
-def nmf_pkg(df):
-
+def vectorize(df):
     vectorizer = TfidfVectorizer(stop_words='english', max_features=1000)
     X = vectorizer.fit_transform(df['articles_joined'])
     words = vectorizer.get_feature_names()
+    return vectorizer, X, words
+
+def get_concepts(df):
+    vectorizer, X, words = vectorize(df)
+
     nmf = NMF(n_components=7)
     nmf.fit(X.toarray())
     W = nmf.transform(X.toarray())
@@ -35,8 +39,20 @@ def nmf_pkg(df):
     for i in range(7):
         index_h = np.argsort(H[i,:])[-10:]
         most_common_words = [words[idx] for idx in index_h]
-
-        index_w = np.argsort(W[:,i], axis=0)[-10:]
         concepts[i] = most_common_words
 
     return concepts
+
+def get_idfs(df):
+    vectorizer, X, words = vectorize(df)
+
+    idfs = vectorizer.idf_
+    words_idfs = list(zip(words, idfs))
+
+    smallest = sorted(words_idfs, key = operator.itemgetter(1))[:20]
+    smallest = pd.Series([features[1] for features in smallest], index = [features[0] for features in smallest])
+    biggest = sorted(words_idfs, key=operator.itemgetter(1), reverse=True)[:20]
+    biggest = sorted(biggest, key = operator.itemgetter(0))
+    biggest = pd.Series([features[1] for features in biggest], index = [features[0] for features in biggest])
+
+    return smallest, biggest
